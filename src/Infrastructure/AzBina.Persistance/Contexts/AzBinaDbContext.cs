1e6623a95ce0686dcdd8553a46ca0f65ab56f346
Farkhad77 <farhadramazanov2006@gmail.com>
2025-05-29 14:09:22 +0400

    DbContext.9.1

---
+++ src/Infrastructure/AzBina.Persistance/Contexts/AzBinaDbContext.cs
@@ -0,0 +1,29 @@
+﻿using AzBina.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzBina.Persistance.Contexts
+{
+    public class AzBinaDbContext : DbContext
+    {
+        public AzBinaDbContext(DbContextOptions<AzBinaDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Bio> Bios { get; set; }
+        public DbSet<Ad> Ads{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Township> Townships{ get; set; }
+        public DbSet<Favorite> Favorites{ get; set; }
+        public DbSet<Image> Images{ get; set; }
+       
+
+    }
+    
+}
