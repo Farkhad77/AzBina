6190ef40b51910fd85a45349b88fa745e664d016
Farkhad77 <farhadramazanov2006@gmail.com>
2025-06-07 16:43:44 +0400

    Repository scrum 12.1

--- src/core/AzBina.Application/Abstracts/Repositories/IRepository.cs
+++ src/core/AzBina.Application/Abstracts/Repositories/IRepository.cs
@@ -10,19 +10,19 @@ namespace AzBina.Persistance.Repositories
 {
     public interface IRepository<T> where T : BaseEntity , new()
     {
-        Task<T> GetByIdAsync(Guid id);
+         Task<T?> GetByIdAsync(Guid id);
         IQueryable<T> GetByFiltered(Expression<Func<T, bool>>? predicate = null,
             Expression<Func<T, object>>[]? include = null,
             bool isTracking= false);
-        IQueryable<T> GetAllFiltered(bool isTracking= false);
+        IQueryable<T> GetAll(bool isTracking= false);
 
         IQueryable<T> GetAllFiltered(Expression<Func<T, bool>>? predicate= null ,
             Expression<Func<T, object>>[]? include=null,
             Expression<Func<T, bool>>? orderBy = null,
             bool isOrderByAsc=true,
             bool isTracking = false);
-        Task<T> SaveChangeAsync();
-        Task<T> AddAsync(T entity);
+        Task SaveChangeAsync();
+        Task AddAsync(T entity);
         void Update(T entity);
         void Delete(T entity);
     }
