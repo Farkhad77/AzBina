b72c44bdf040beadbe035841ed3b4d395faf3a2b
Farkhad77 <farhadramazanov2006@gmail.com>
2025-06-15 17:19:37 +0400

    City

---
+++ src/Presentation/AzBina.WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,75 @@
+﻿using AzBina.Application.Abstracts.Services;
+using AzBina.Application.DTOs.CategoryDtos;
+using AzBina.Application.DTOs.CityDtos;
+using AzBina.Application.Shared;
+using AzBina.Persistance.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzBina.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+       
+            public CitiesController(ICityService cityService)
+            {
+                _cityService = cityService;
+            }
+
+            private ICityService _cityService { get; }
+
+
+            // GET: api/<CitiesController>
+            [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CitiesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CitiesController>
+        [HttpPost]
+        [ProducesResponseType(typeof(BaseResponse<CityUpdateDto>), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(BaseResponse<string>), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(BaseResponse<string>), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> Post([FromBody] CityCreateDto dto)
+        {
+            var result = await _cityService.AddAsync(dto);
+            return StatusCode((int)result.StatusCode, result);
+        }
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CitiesController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] CityUpdateDto dto)
+        {
+            var result = await _cityService.UpdateAsync(dto);
+            return StatusCode((int)result.StatusCode, result);
+        }
+
+
+        // DELETE api/<CitiesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        [HttpGet("search")]
+        public async Task<IActionResult> Search(string query)
+        {
+            var result = await _cityService.GetByNameSearchAsync(query);
+            return Ok(result);
+        }
+    }
+}
---
+++ src/core/AzBina.Application/Abstracts/Repositories/ICityRepository.cs
@@ -0,0 +1,14 @@
+﻿using AzBina.Domain.Entities;
+using AzBina.Persistance.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzBina.Application.Abstracts.Repositories;
+
+public interface ICityRepository:IRepository<Domain.Entities.City>
+{
+    Task<List<City>> GetByNameSearchAsync(string namePart);
+}
