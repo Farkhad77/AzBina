4e9f1a92eb5220272fc254ba588ba09c6fcdd02f
Farkhad77 <farhadramazanov2006@gmail.com>
2025-06-26 15:28:44 +0400

    Authorization

---
+++ src/Presentation/AzBina.WebAPI/Controllers/RolesController.cs
@@ -0,0 +1,24 @@
+﻿using AzBina.Application.Shared.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzBina.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        // GET: api/<RolesController>
+        [HttpGet("permissions")]
+    
+        public IActionResult GetAllPermissions()
+        {
+            var permissions = PermissionHelper.GetAllPermissions();
+            return Ok(permissions);
+
+        }
+        
+
+    }
+}
